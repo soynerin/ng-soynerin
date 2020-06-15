1d7588d62c00050c58c732ae9ee44d62584ad848
Nerin <neri.agustin.es@gmail.com>
2020-06-14 21:08:42 -0300

    Section About completa

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  edad: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.edad = moment().diff('1989-05-09', 'years');
   }
 
 }
