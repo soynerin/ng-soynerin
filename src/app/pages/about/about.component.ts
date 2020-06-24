580d525a4feeec2c23fb77ff284f000a1f6518df
soynerin <neri.agustin.es@outlook.com>
2020-06-24 19:27:54 -0300

    Rediseño final

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -9,11 +9,65 @@ import * as moment from 'moment';
 export class AboutComponent implements OnInit {
 
   edad: number;
+  visitas: any = {
+    countTo: 1,
+    from: 0,
+    duration: 3
+  };
+  tazasDeCafe: any;
+  proyectosTerminados: any;
+  clientesFelices: any;  
 
-  constructor() { }
+  constructor() { 
+    // this.contadorVisitas();
+    this.contadorTazasCafe();
+    this.contadorProyectosTerminados();
+    this.contadorClientesFelices();    
+  }
 
   ngOnInit(): void {
     this.edad = moment().diff('1989-05-09', 'years');
   }
+
+  // contadorVisitas() {
+
+  //   countapi.get('soynerin.github.io', 'counter').then((result: any) => {
+
+  //     this.visitas = {
+  //       countTo: result.value,
+  //       from: 0,
+  //       duration: 3
+  //     };
+  //   });
+
+  // }
+
+  contadorTazasCafe() {
+    const primerDiaTrabajo = moment('2015-01-09');
+    const hoy = moment();
+    const dias = hoy.diff(primerDiaTrabajo, 'days');
+
+    this.tazasDeCafe = {
+      countTo: dias,
+      from: 0,
+      duration: 10
+    };
+  }
+
+  contadorProyectosTerminados() {
+    this.proyectosTerminados = {
+      countTo: 2,
+      from: 0,
+      duration: 5
+    };
+  }
+
+  contadorClientesFelices() {
+    this.clientesFelices = {
+      countTo: 1,
+      from: 0,
+      duration: 5
+    };
+  }  
 
 }
--- src/assets/js/arshia.js
+++ src/assets/js/arshia.js
@@ -14,22 +14,16 @@ var $root = $('html, body');
 $(document).ready(function () {
 
     "use strict";
-    
     colorScheme();
-    smoothScroll();
-    bgBackground();
-    colorFull();
-    borderColor();
     ColorPallet();
-    swiperSlider();
-    sliderOwlCarousel();
-    // PillMenuToggler();
-    // menuToggler();
-    typedJS();
+    PillMenuToggler();
+    menuToggler();
+    // typedJS();
+    skills();
+    // countUp();
     portfolioPopup();
     clientCarousel();
     BlogCarousel();
-    mapInit();
 
 });
 
@@ -37,19 +31,7 @@ $window.on("load", (function() {
     $("#overlayer").delay(500).fadeOut('slow');
     $(".loader").delay(1000).fadeOut('slow');
     portfolioIsotop();
-    headerSticky();
-    ChangeColor();
-
 }));
-$window.on('scroll', function () {
-    headerSticky();
-    skills();
-    ChangeColor();
-    
-    
-});
-
-
 
 /*-------------------------
         Color Scheme
@@ -63,111 +45,46 @@ function colorScheme(){
     });
 }
 
-/*-------------------------
-        SMOOTH SCROLL
--------------------------*/
-function smoothScroll(){
-
-    "use strict";
-
-    $('.header .navbar-nav a').on('click', function(event) {
-        var $anchor = $(this);
-        $root.stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 60
-        }, 1500, 'easeInOutQuart');
-        event.preventDefault();
-        $(".navbar-collapse").collapse('hide');
-    });
-}
-
-/*-------------------------
-        HEADER STICKY
--------------------------*/
-function headerSticky(){
-
-    "use strict";
-
-    if ($window.scrollTop() > 100) {
-        $('#header').addClass('header-sticky');
-    } else {
-        $('#header').removeClass('header-sticky');
-    }
-}
-/*-------------------------
-        ChangeColor
--------------------------*/
-function ChangeColor(){
-
-    "use strict";
-
-    var top1 = $('#hero').offset().top;
-    var top2 = $('#about').offset().top;
-
-    if ($window.scrollTop() == top1) {
-        $('.special-section').css('color', '#fff');
-      } else {
-        $('.special-section').css('color', '#1a1a1a');
-      } 
-    
-}
-
-
-
-
-
- 
-  
-
 
 /*-------------------------
         ColorFull Demo
 -------------------------*/
 
-function bgBackground() {
+var list = document.getElementsByClassName('data-background');
 
-    "use strict";
-
-    var list = document.getElementsByClassName('data-background');
-
-    for (var i = 0; i < list.length; i++) {
-        var color = list[i].getAttribute('data-color');
-        list[i].style.backgroundColor = "" + color + "";
-    }
-}
-
+     for (var i = 0; i < list.length; i++) {
+       var color = list[i].getAttribute('data-color');
+       list[i].style.backgroundColor = "" + color + "";
+     }
 
      
-function colorFull() {
-    var allDivs = document.getElementsByClassName('data-text-color');
+var allDivs = document.getElementsByClassName('data-text-color');
 
     for( var i =0; i < allDivs.length; ++i )
     {
         var color = allDivs[i].getAttribute('data-color');
         allDivs[i].style.color = "" + color + "";
     }
-}
 
+    
+var allDivs = document.getElementsByClassName('timeline-border');
 
-function borderColor() {
-    var allDivs = document.getElementsByClassName('timeline-border');
-
-    for( var i =0; i < allDivs.length; ++i )
-    {
-        var color = allDivs[i].getAttribute('data-color');
-        allDivs[i].style.borderLeftColor = "" + color + "";
+for( var i =0; i < allDivs.length; ++i )
+{
+    var color = allDivs[i].getAttribute('data-color');
+    allDivs[i].style.borderLeftColor = "" + color + "";
     }
-}
    
     
 /*-----------------------------------------------------------------------------
                                    FUNCTIONS
 -----------------------------------------------------------------------------*/
-// $("#menu > li a").on("click" , function () {
-//     $("#main > section.active, #menu > li a").removeClass("active");
-//     $(this).addClass('active');
-//     var $id = $(this).attr('href');
-//     $('#main').children($id).addClass('active');
-// });
+$("#menu > li a").on("click" , function () {
+    $("#main > section.active, #menu > li a").removeClass("active");
+    $(this).addClass('active');
+    var $id = $(this).attr('href');
+    $('#main').children($id).addClass('active');
+});
 
 
 $(".to-contact").on('click', function(){
@@ -184,138 +101,69 @@ function ColorPallet() {
     "use strict";
 
     $("ul.pattern .color1").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/green-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/green-color.css")
     });
     $("ul.pattern .color2").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/yellow-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/yellow-color.css")
     });
     $("ul.pattern .color3").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/golden-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/golden-color.css")
     });
     $("ul.pattern .color4").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/sky-blue-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/sky-blue-color.css")
     });
     $("ul.pattern .color5").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/blue-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/blue-color.css")
     });
     $("ul.pattern .color6").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/purple-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/purple-color.css")
     });
     $("ul.pattern .color7").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/orange-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/orange-color.css")
     });
     $("ul.pattern .color8").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/pink-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/pink-color.css")
     });
     $("ul.pattern .color9").click(function () {
-        return $("#option-color").attr("href", "assets/css/color/red-color.css"), !1
+        return $("#option-color").attr("href", "assets/css/color/red-color.css")
     });
     $("#color-switcher .pallet-button").click(function () {
-        return $("#color-switcher .color-pallet").toggleClass('show'), !1
+        $("#color-switcher .color-pallet").toggleClass('show')
     })
 
 }
-/*-----------------------------
-     HERO SWIPER SLIDER
-------------------------------*/
-function swiperSlider(){
 
-    "use strict";
-
-    if($(".swiper-container").length){
-        var swiper = new Swiper('.swiper-container', {
-            effect: "slide",
-            allowTouchMove: 'false',
-            touchRatio: 0,
-            threshold: 992,
-            autoplay: {
-                delay: 5000,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
-
-        var textSwiper = new Swiper('.text-swiper', {
-            effect: "fade",
-            allowTouchMove: 'false',
-            touchRatio: 0,
-            threshold: 992,
-            autoplay: {
-                delay: 5000,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
-
-        $(".hero-item-image").css('background', function () {
-            var bg = ('url(' + $(this).data("image-src") + ') no-repeat center');
-            return bg;
-        });
-        var $fullscreen = $(".hero-04, .hero-swiper, .hero-text, .hero-images");
-        $fullscreen.css("height", $window.height());
-    }
-}
-/*-----------------------------
-      SLIDER OWL CAROUSEL
-------------------------------*/
-function sliderOwlCarousel(){
-    "use strict";
-
-    $('.hero .owl-carousel').owlCarousel({
-        loop:true,
-        items: 1,
-        nav: false,
-        dots: false,
-        autoplay:true,
-        touchDrag: true,
-        smartSpeed: 5000,
-        animateOut: 'fadeOut',
-        autoplayHoverPause: true,
-    });
-    $('#hero-slider').on("translate.owl.carousel", function(){
-        setTimeout(function(){
-            $('.hero-slide').removeClass("zoom");
-        }, 1000)
-    });
-    $('#hero-slider').on("translated.owl.carousel", function(){
-        $('.owl-item.active .hero-slide').addClass("zoom");
-    });
-}
 // /*-------------------------
 //     MENU TOGGLER
 // -------------------------*/
-// function PillMenuToggler() {
+function PillMenuToggler() {
 
-//     "use strict";
-//     $(".overlay-menu-toggler").click(function(){
-//         $(".overlay-menu").addClass("show");
-//     });
-//     $(".overlay-menu").click(function(){
-//         $(this).removeClass("show");
-//     });
-// }
+    "use strict";
+    $(".overlay-menu-toggler").click(function(){
+        $(".overlay-menu").addClass("show");
+    });
+    $(".overlay-menu").click(function(){
+        $(this).removeClass("show");
+    });
+}
 
-// function  menuToggler() {
-//     "use strict";
-//     var $menuToggler = $(".menu-toggler");
-//     var $header = $('header');
-//     $menuToggler.click(function () {
-//         $(this).toggleClass('open').find('i').toggleClass('lni-menu lni-close ');
-//         $header.toggleClass('open');
-//         $('.color-scheme, .pallet-button, .color-pallet').toggleClass('hide');
-//     });
-//     if ($window.width() < 1200){
-//         $('header li a').click(function () {
-//             $header.removeClass('open');
-//             $('.color-scheme, .pallet-button, .color-pallet').toggleClass('hide');
-//             $menuToggler.removeClass('open').find('i').removeClass('lni-close').addClass('lni-menu');
-//         })
-//     }
-// }
+function  menuToggler() {
+    "use strict";
+    var $menuToggler = $(".menu-toggler");
+    var $header = $('header');
+    $menuToggler.click(function () {
+        $(this).toggleClass('open').find('i').toggleClass('lni-menu lni-close ');
+        $header.toggleClass('open');
+        $('.color-scheme, .pallet-button, .color-pallet').toggleClass('hide');
+    });
+    if ($window.width() < 1200){
+        $('header li a').click(function () {
+            $header.removeClass('open');
+            $('.color-scheme, .pallet-button, .color-pallet').toggleClass('hide');
+            $menuToggler.removeClass('open').find('i').removeClass('lni-close').addClass('lni-menu');
+        })
+    }
+}
 /*-------------------------
         TYPED JS
 -------------------------*/
@@ -338,20 +186,11 @@ function typedJS() {
 function skills() {
 
     "use strict";
-
-    var scroll = $window.scrollTop();
-    var skillsDiv = $('#skills');
-    if(skillsDiv.length > 0){
-        var winH = $window.height(),
-            skillsT = skillsDiv.offset().top;
-        if (scroll + winH > skillsT) {
-            $('.skillbar').each(function () {
-                $(this).find('.skillbar-bar').animate({
-                    width: $(this).attr('data-percent')
-                }, 6000);
-            });
-        }
-    }
+    $('.skillbar').each(function () {
+        $(this).find('.skillbar-bar').animate({
+            width: $(this).attr('data-percent')
+        }, 6000);
+    });
 }
 
 /*-------------------------
@@ -434,13 +273,14 @@ function clientCarousel() {
         smartSpeed: 700,
         autoplayHoverPause: false,
         responsiveClass: true,
+        rtl: true,
         responsive: {
             0: {
                 items: 1,
                 nav: false
             },
             992: {
-                items: 2,
+                items: 3,
                 nav: false,
             },
 
@@ -467,157 +307,6 @@ function BlogCarousel() {
         autoplayHoverPause: true,
 
     });
-}
-
-/*-------------------------
-          GOOGLE Map
-  -------------------------*/
-  function mapInit() {
-
-    "use strict";
-    var myMap = $('#my-map');
-
-    if(myMap.length){
-        var lat = myMap.data("location-lat");
-        var lng = myMap.data("location-lng");
-        var options = {
-            center: new google.maps.LatLng(lat, lng),
-            zoom: 7,
-            mapTypeControl: true,
-            gestureHandling: 'cooperative',
-            panControl: false,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-                position: google.maps.ControlPosition.TOP_LEFT
-            },
-            scaleControl: false,
-            styles: [{
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#576ee9"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 20
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 29
-                }, {
-                    "weight": 0.2
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "road.local",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#d5d5d5"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "color": "#f8f8f8"
-                }, {
-                    "lightness": 25
-                }]
-            }, {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "saturation": 36
-                }, {
-                    "color": "#222222"
-                }, {
-                    "lightness": 30
-                }]
-            }, {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 19
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 10
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 17
-                }, {
-                    "weight": 1.2
-                }]
-            }],
-        };
-        var map = new google.maps.Map(document.getElementById('my-map'), options);
-        var marker1 = new google.maps.Marker({
-            position: map.getCenter(),
-            title: $('title').text(),
-            icon: myMap.data("location-icon"),
-            animation: google.maps.Animation.BOUNCE
-        });
-        marker1.setMap(map);
-    }
 }
 /*-------------------------
      AJAX CONTACT FORM
