4cc4398bd76c5e5ac21a9386e42dca2323d6727b
soynerin <neri.agustin.es@outlook.com>
2020-06-26 17:28:38 -0300

    Se incorpora manejo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroComponent } from './pages/hero/hero.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 const routes: Routes = [
-  { path: '', component: HeroComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: 'hero', component: HeroComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'works', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'hero' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  isLoading = true;
+
   constructor() { }
 
   ngOnInit(): void {
